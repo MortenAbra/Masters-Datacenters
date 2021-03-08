a71f864eaba65c2026ceea0f16e39cb528c67dd1
Wolder <nikolajwolder@gmail.com>
2021-03-08 12:10:44 +0100

    Created Maven Project

---
+++ EnergiDataService test/Java/energinet/src/main/java/drenerginet/App.java
@@ -0,0 +1,13 @@
+package drenerginet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
