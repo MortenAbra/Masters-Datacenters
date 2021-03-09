b86925fad86dbe2be6ab558c7a39dd248bdfede8
MortenAbra <sezure@gmail.com>
2021-03-09 23:15:53 +0100

    Refactoring

--- EnergiDataService test/Java/energinet/src/main/java/drenerginet/EnergiDataFetcher.java
+++ EnergiDataService test/Java/energinet/src/main/java/drenerginet/EnergiDataFetcher.java
@@ -4,39 +4,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
 import org.json.JSONObject;
 
 
-public class EnergiDataFetcher {
+public class EnergiDataFetcher extends FileHandler {
 
     private InputStream input;
     private BufferedReader reader;
-    private JSONObject jsonObject;
     private FileHandler fileSaver;
+    private final OkHttpClient client = new OkHttpClient();
+    
 
-    public EnergiDataFetcher(){}
+    public EnergiDataFetcher(){
+        fileSaver = new FileHandler();
+    }
 
     //https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT%20%22HourUTC%22,%20%22HourDK%22,%20%22PriceArea%22,%20%22SpotPriceDKK%22,%20%22SpotPriceEUR%22%20FROM%20%22elspotprices%22%20ORDER%20BY%20%22HourUTC%22%20DESC%20LIMIT%20100
 
-    public JSONObject fetchAPIData(String apiUrl) throws MalformedURLException, IOException{
-        String url = apiUrl;
-        fileSaver = new FileHandler();
-        input = new URL(url).openStream();
+    public JSONObject fetchAPIData(String url) throws MalformedURLException, IOException{
+        setUrl(url);
+        input = new URL(getUrl()).openStream();
+
         reader = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
-        String inputData = fileSaver.readChaString(reader);
+        String inputData = readChaString(reader);
+        JSONObject data = convertToJson(inputData);
+        saveDataToFile(data, "SpotPrice.json", "src\\main\\java\\drenerginet\\Results\\");
 
-        
-        fileSaver.saveDataToFile(fileSaver.convertToJson(inputData), "SpotPrice.json", "src\\main\\java\\drenerginet\\Results\\");
-
-        return fileSaver.convertToJson(inputData);
-    }
-
-    public void getSpotPrice(String string) {
-    }
-    
+        return data;
+    }   
 }
--- EnergiDataService test/Java/energinet/src/main/java/drenerginet/FileHandler.java
+++ EnergiDataService test/Java/energinet/src/main/java/drenerginet/FileHandler.java
@@ -3,7 +3,7 @@ package drenerginet;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
-
+import java.sql.Timestamp;
 import org.json.JSONObject;
 
 
@@ -12,19 +12,24 @@ public class FileHandler {
 
     private FileWriter file;
     private JSONObject jsonObject;
+    private String url;
+    private Timestamp time;
+
 
     public FileHandler(){
-
+        time = new Timestamp(System.currentTimeMillis());
     }
     
     public void saveDataToFile(JSONObject object, String fileName, String path) throws IOException{
         file = new FileWriter(path + fileName);
         file.write(object.toString());
         file.close();
+        TextOutPut("DATA SAVED TO FILE");
     }
 
     public JSONObject convertToJson(String inputData){
         jsonObject = new JSONObject(inputData);
+        TextOutPut("DATA CONVERTED TO JSON");
         return jsonObject;
     }
 
@@ -36,5 +41,26 @@ public class FileHandler {
         }
         return sb.toString();
     }
+
+    public void TextOutPut(String inpuString){
+        System.out.print(time + ": " + inpuString + '\n');
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public void setJsonObject(JSONObject jsonObject){
+        this.jsonObject = jsonObject;
+    }
+
+    public JSONObject getJsonObject(){
+        return jsonObject;
+    }
+
     
 }
