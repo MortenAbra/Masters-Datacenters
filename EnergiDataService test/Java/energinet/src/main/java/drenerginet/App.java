ae1841343eae514c7d5ca6e820c7332a338ee07a
MortenAbra <sezure@gmail.com>
2021-03-12 16:48:35 +0100

    Refactoring etc

--- EnergiDataService test/Java/energinet/src/main/java/drenerginet/App.java
+++ EnergiDataService test/Java/energinet/src/main/java/drenerginet/App.java
@@ -2,11 +2,8 @@ package drenerginet;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.sql.Date;
 import java.util.concurrent.Future;
-
 import org.json.simple.JSONObject;
-
 import drenerginet.Models.Record;
 import drenerginet.Models.Result;
 import drenerginet.Models.Workload;
@@ -19,24 +16,27 @@ import drenerginet.Pinger.DockerPinger;
 */
 
 public class App {
-    static String workloadPath = "energinet\\src\\main\\java\\drenerginet\\Results\\workloads.json";
-    static String outputPath = "energinet\\src\\main\\java\\drenerginet\\Results\\output.csv";
-    static String apiUrl = "https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT%20%22HourUTC%22,%20%22HourDK%22,%20%22PriceArea%22,%20%22SpotPriceDKK%22,%20%22SpotPriceEUR%22%20FROM%20%22elspotprices%22%20ORDER%20BY%20%22HourUTC%22%20DESC%20LIMIT%20100";
     static FileHandler fh = new FileHandler();
     static Record record;
     static Result res = new Result();
     public static void main(String[] args) throws Exception {
-        EnergiDataFetcher edf = new EnergiDataFetcher();
-        DockerPinger pinger = new DockerPinger();
-        
+        //DockerPinger pinger = new DockerPinger();
+        ExecutionManager em = new ExecutionManager();
+
+        em.ExecuteJSONTask(em.getRunnableList());
+
+
+
+        /*
         while(true){
-            Future future = new EnergiDataFetcher().fetchData(apiUrl);
+            Future future = new EnergiDataFetcher().fetchData(fh.getAPIURL(), "somename.json");
             while(!future.isDone()){
                 fh.TextOutPut("Fetching data...");
                 Thread.sleep(10000);
             }
             JSONObject data = (JSONObject) future.get();
             
+            /*
             for (int i = 0; i < data.size(); i++) {
                 Date hourdk = (Date) data.get("HourDK");
                 Date hourutc = (Date) data.get("HourUTC");
@@ -47,13 +47,16 @@ public class App {
                 res.getRecords().add(record);
             }
             fh.TextOutPut(res.toString());
+            
                     
-            ArrayList<Workload> workloads = edf.addJSONWorkloadToList(workloadPath);
+            ArrayList<Workload> workloads = fh.addJSONWorkloadToList();
 
-            for (int i = 0; i < edf.getWorkloadList().size(); i++) {
-                pinger.pingHost(workloads.get(i), new FileWriter(outputPath));
+            for (int i = 0; i < fh.getWorkloadList().size(); i++) {
+                pinger.pingHost(workloads.get(i), new FileWriter(fh.getOUTPUTPATH()));
                 Thread.sleep(1000);
             }
+        
         }
+        */
     }
 }
