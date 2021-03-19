bf95e2a84ea643d201a83d0d3867a6658d2b3624
MortenAbra <sezure@gmail.com>
2021-03-19 19:02:59 +0100

    Removed unfinished code

--- MasterController/drenerginet/src/main/java/mastercontroller/AppController.java
+++ MasterController/drenerginet/src/main/java/mastercontroller/AppController.java
@@ -8,17 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class AppController {
-    @FXML private Text someText;
-    @FXML private Button btn = new Button();
-    private App app;
-
-    public AppController() throws IOException{
-        app = new App();
-    }
-
-    @FXML private void getBtn(Button button){
-        
-        
-    }
+    
     
 }
