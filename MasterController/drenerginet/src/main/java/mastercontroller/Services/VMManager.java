7dbcfd503c20ac088182266a684cc4543f6c908d
MortenAbra <sezure@gmail.com>
2021-03-20 19:04:04 +0100

    added get vm features

--- MasterController/drenerginet/src/main/java/mastercontroller/Services/VMManager.java
+++ MasterController/drenerginet/src/main/java/mastercontroller/Services/VMManager.java
@@ -6,15 +6,18 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import com.google.gson.JsonObject;
 
 import mastercontroller.FileHandler.FileHandler;
 import mastercontroller.Models.Workload;
 
-public class DockerPinger {
+public class VMManager {
 
     private FileHandler fh;
 
-    public DockerPinger(){
+    public VMManager(){
         fh = new FileHandler();
     }
 
@@ -81,6 +84,42 @@ public class DockerPinger {
         writer.append("\n");
         writer.flush();
         writer.close();
+    }
+
+    private ArrayList<JsonObject> findActiveVMs(ArrayList<JsonObject> vmList){
+        ArrayList<JsonObject> activeVMs = new ArrayList<>();
+        for (JsonObject vm : vmList) {
+            fh.TextOutPut("VM: " + vm.get("name") + " is - " + vm.get("active").getAsString());
+            if(vm.get("active").getAsBoolean() == true){
+                if(activeVMs.contains(vm)){
+                    fh.TextOutPut("VM already in available list...");
+                } else {
+                    fh.TextOutPut("Adding vm to available list...");
+                    activeVMs.add(vm);
+                }
+            } else {
+                fh.TextOutPut(vm.get("name").getAsString() + ": Unavailable...");
+            }            
+        }
+        return activeVMs;
+    }
+
+    private JsonObject getActiveVMObject(ArrayList<JsonObject> activeVMList){
+        JsonObject readyVmObject = activeVMList.get(0);
+        readyVmObject.getAsJsonObject("workload").addProperty("active", true);
+        return readyVmObject;
+    }
+
+    private String getSingleActiveVMsIP(JsonObject object){
+        return object.get("ip").getAsString();
+    }
+
+    private ArrayList<String> getActiveVMsIPAsList(ArrayList<JsonObject> vmList){
+        ArrayList<String> vmIpList = new ArrayList<>();
+        for (JsonObject object : vmList) {
+            vmIpList.add(object.get("ip").getAsString());
+        }
+        return vmIpList;
     }
     
 }
