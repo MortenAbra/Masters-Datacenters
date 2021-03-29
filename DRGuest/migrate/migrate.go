a984f033cb3cbf9a0ef96b2a822150b2dfaa97af
wolder <nikolajwolder@gmail.com>
2021-03-29 21:18:54 +0200

    New Revision of DRGuest Go controller

--- DRGuest/migrate/migrate.go
+++ DRGuest/migrate/migrate.go
@@ -2,10 +2,13 @@ package migrate
 
 import (
 	"DRGuest/guest"
-	"DRGuest/workload"
 )
 
-type Migration struct {
-	workload    workload.Workload
-	targetGuest guest.Guest
+type Migrate struct {
+	Name        string      `json:"wl_name"`
+	TargetGuest guest.Guest `json:"target_guest"`
+}
+
+func New(_name string, guest guest.Guest) Migrate {
+	return Migrate{Name: _name, TargetGuest: guest}
 }
