98bdc20aec21aec59446f00454047f9e7b314fc7
wolder <nikolajwolder@gmail.com>
2021-04-04 19:59:15 +0200

    Added Scripts and renamed Workloads example to Useful Stuff

---
+++ Useful Stuff/setup_drguest_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+mkdir ~/REMOVE
+cd ~/REMOVE
+sudo wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+sudo dpkg -i docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+# Daemon environment will run automaticly
+
+# Run docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# docker checkpoint is an experimental feature, so you should enable docker experimental feature
+sudo touch /etc/docker/daemon.json
+echo "{\"experimental:\":true}" >> /etc/docker/daemon.json
+sudo systemctl restart docker
+
+sudo docker version
