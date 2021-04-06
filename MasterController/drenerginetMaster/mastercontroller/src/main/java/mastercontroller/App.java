9ee4819c790660e6fd3e356ff75599da216378fc
MortenAbra <sezure@gmail.com>
2021-04-06 16:18:10 +0200

    Temp gui setup

--- MasterController/drenerginetMaster/mastercontroller/src/main/java/mastercontroller/App.java
+++ MasterController/drenerginetMaster/mastercontroller/src/main/java/mastercontroller/App.java
@@ -2,72 +2,192 @@ package mastercontroller;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-import mastercontroller.Models.Workload;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
 import mastercontroller.Services.VMManager;
 
 /**
  * Hello world!
  *
  */
-public class App extends JFrame{
+public class App extends JFrame {
 
-    private JFrame frame;
     private JPanel panel;
+    private JLabel vmNameLabel;
+    private JLabel vmProperties;
+    private JLabel vmIP;
+    private JLabel vmStatus;
+    private JLabel vmMigrationLabel;
+    private JComboBox cmbAvailableVMList;
+    private JButton btConfirmMigration;
+    private JList lsVmList;
+    private JScrollPane scpVmList;
+    private GridBagLayout gbLayout;
+    private GridBagConstraints gbConstraints;
+    private ComponentGenerator generator;
+    private VMManager manager;
 
-    public App(){
-        frame = new JFrame("VM Manager");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        panel = new JPanel();
-    }
 
-    
-    
-    public static void main( String[] args )
-    {
-        App app = new App();
-        ComponentGenerator generator = new ComponentGenerator();
-        VMManager vmManager = new VMManager();
-
+    public App() {
+        manager = new VMManager();
+        generator = new ComponentGenerator();
         try {
-            vmManager.addJSONWorkloadToList();
+            manager.addJSONWorkloadToList();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        generator.generateComponents(generator.getComponentList(), app.getFrame(), vmManager.getWorkloadList());
+        addComponenets(generator);
+    }
+
+    private void addComponenets(ComponentGenerator generator){
+        String[] dataVmList = { "Chocolate", "Ice Cream", "Apple Pie" };
+        String[] dataAvailableVMList = { "Chocolate", "Ice Cream", "Apple Pie" };
+
+        String[] test = generator.fillVMList(manager.getWorkloadList());
+
+
         
+        panel = new JPanel();
+        this.add(panel);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(800,400);
+        this.setVisible(true);
+        this.setResizable(true);
+        this.setTitle("VM Manager");
+        panel.setBorder(BorderFactory.createTitledBorder("VM List"));
+        gbLayout = new GridBagLayout();
+        gbConstraints = new GridBagConstraints();
+        panel.setLayout(gbLayout);
+
+        lsVmList = generator.addList(test);
+        scpVmList = generator.addScrollPane(lsVmList);
+        vmNameLabel = generator.addLabel("VM Name: ");
+        vmProperties = generator.addLabel("VM Properties");
+        vmIP = generator.addLabel("VM IP:");
+        vmStatus = generator.addLabel("VM Status");
+        vmMigrationLabel = generator.addLabel("Migration To: ");
+        cmbAvailableVMList = generator.addComboBox(dataAvailableVMList);
+        btConfirmMigration = generator.addButton("Migrate");
+        
+        Setup(generator, gbLayout, gbConstraints);
+    }
+
+    private void Setup(ComponentGenerator generator, GridBagLayout gbLayout, GridBagConstraints gbConstraints) {
+        
+        gbConstraints.gridx = 1;
+        gbConstraints.gridy = 1;
+        gbConstraints.gridwidth = 2;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(vmNameLabel, gbConstraints);
+
+
+         
+        gbConstraints.gridx = 1;
+        gbConstraints.gridy = 0;
+        gbConstraints.gridwidth = 2;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(vmProperties, gbConstraints);
+
+
+         
+        gbConstraints.gridx = 1;
+        gbConstraints.gridy = 2;
+        gbConstraints.gridwidth = 2;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(vmIP, gbConstraints);
+
+
+         
+        gbConstraints.gridx = 1;
+        gbConstraints.gridy = 3;
+        gbConstraints.gridwidth = 2;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(vmStatus, gbConstraints);
+
+
+         
+        gbConstraints.gridx = 1;
+        gbConstraints.gridy = 4;
+        gbConstraints.gridwidth = 1;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(vmMigrationLabel, gbConstraints);
 
 
         
+         
+        gbConstraints.gridx = 2;
+        gbConstraints.gridy = 4;
+        gbConstraints.gridwidth = 1;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 0;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(cmbAvailableVMList, gbConstraints);
+
+
+        gbConstraints.gridx = 1;
+        gbConstraints.gridy = 5;
+        gbConstraints.gridwidth = 2;
+        gbConstraints.gridheight = 1;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 0;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(btConfirmMigration, gbConstraints);
+
+
+        gbConstraints.gridx = 0;
+        gbConstraints.gridy = 0;
+        gbConstraints.gridwidth = 1;
+        gbConstraints.gridheight = 6;
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(scpVmList, gbConstraints);
 
 
     }
 
+    public static void main(String[] args) {
+        App app = new App();
+        app.generator.generateComponents(app.generator.getComponentList(), app.getPanel());
 
 
-    /**
-     * @return the frame
-     */
-    public JFrame getFrame() {
-        return frame;
     }
-
-
-
-    /**
-     * @param frame the frame to set
-     */
-    public void setFrame(JFrame frame) {
-        this.frame = frame;
-    }
-
-
 
     /**
      * @return the panel
@@ -75,8 +195,6 @@ public class App extends JFrame{
     public JPanel getPanel() {
         return panel;
     }
-
-
 
     /**
      * @param panel the panel to set
--- MasterController/drenerginetMaster/mastercontroller/src/main/java/mastercontroller/ComponentGenerator.java
+++ MasterController/drenerginetMaster/mastercontroller/src/main/java/mastercontroller/ComponentGenerator.java
@@ -5,26 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import mastercontroller.Models.Workload;
+import mastercontroller.Services.VMManager;
 
 public class ComponentGenerator extends JComponent {
 
 
     private ArrayList<JComponent> componentList;
-    private JTextArea vmListTextArea;
-    private JLabel vmListLabel;
-    private JLabel vmProperties;
-    private JLabel vmName;
-    private JLabel vmStatus;
-    private JLabel vmIP;
-    private JLabel vmPort;
 
     public ComponentGenerator() {
         componentList = new ArrayList<>();
@@ -34,6 +31,12 @@ public class ComponentGenerator extends JComponent {
     public JButton addButton(String title, int x, int y, int width, int height){
         JButton button = new JButton(title);
         button.setBounds(x, y, width, height);
+        componentList.add(button);
+        return button;
+    }
+
+    public JButton addButton(String title){
+        JButton button = new JButton(title);
         componentList.add(button);
         return button;
     }
@@ -52,43 +55,62 @@ public class ComponentGenerator extends JComponent {
         return label;
     }
 
+    public JLabel addLabel(String title){
+        JLabel label = new JLabel(title);
+        componentList.add(label);
+        return label;
+    }
+
+    public JComboBox addComboBox(String[] list){
+        JComboBox box = new JComboBox(list);
+        componentList.add(box);
+        return box;
+    }
+
     public JTextArea addTextArea(int rows, int columns){
         JTextArea textArea = new JTextArea(rows, columns);
         componentList.add(textArea);
         return textArea;
     }
 
-    public JScrollPane addScrollPane(JTextArea textarea){
-        JScrollPane scrollPane = new JScrollPane(textarea);
+    public JList addList(String[] listdata){
+        JList list = new JList(listdata);
+        componentList.add(list);
+        return list;
+    }
+
+    public JScrollPane addScrollPane(JList list){
+        JScrollPane scrollPane = new JScrollPane(list);
         componentList.add(scrollPane);
         return scrollPane;
     }
 
-    public void initComponents(){
-        vmListTextArea = addTextArea(1, 1);
-        addScrollPane(vmListTextArea);
-        addLabel("VM List", 5, 5, 50, 30);
-    }
 
-    public void fillVMList(ArrayList<Workload> workloadList){
-        for (Workload workload : workloadList) {
-            vmListTextArea.append(workload.toString() + '\n');
-            //TextOutPut("Workload added to text area...");
+    public String[] fillVMList(ArrayList<Workload> workloadList){
+        ArrayList<String> vms = new ArrayList<>();
+        if(workloadList.isEmpty()){
+            VMManager manager = new VMManager();
+            try {
+                manager.addJSONWorkloadToList();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            for (Workload workload : workloadList){
+                System.out.println(workload.toString());
+                vms.add(workload.toString());
+            }
         }
+        return vms.toArray(String[]::new);
     }
 
-    public void generateComponents(ArrayList<JComponent> list, JFrame frame, ArrayList<Workload> workloadList){
-        initComponents();
+    public void generateComponents(ArrayList<JComponent> list, JPanel frame){
         for (JComponent jComponent : list) {
+            System.out.println(jComponent.getClass());
             frame.add(jComponent);
             //TextOutPut(jComponent.getClass().toGenericString() + " have been added to frame...");
             frame.setVisible(true);
-        }
-        if(!workloadList.isEmpty()){
-            //TextOutPut(String.valueOf(workloadList.size()));
-            fillVMList(workloadList);
-        } else {
-            //TextOutPut("Workload list is empty...");
         }
     }
 
